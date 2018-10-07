ee0a231fe3e2703d9c1984a6dcd9260dd9f2424f
nathanielpascual <nath_pascual@yahoo.com>
2018-10-07 15:02:14 +0800

    ADD PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash')
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
-var {ObjectID} = require('mongodb');
-
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
@@ -64,33 +64,66 @@ app.get('/todos/:id',(req,res)=>{
         res.status(400).send();
     });
     
-    app.delete('/todos/:id',(req,res) => {
-
-        //get the id
-        var id = req.params.id;
-        //validate the id -> validate the id? return 404
-        if(!ObjectID.isValid(id))
-        {
-            res.status(404).send();
-        }
-
-        //remove todo by id
-
-        Todo.findOneAndDelete(id).then((todo)=> {
-            //success
-            //if no doc, send 404
-            if(!todo)
-                return res.status(404).send();
-             //if doc, send doc back with 200
-            res.send({todo});
-          
-        }).catch((e)=> {
-            res.status(400).send();
-        });
-
-        
-    });
    
+});
+
+app.delete('/todos/:id',(req,res) => {
+
+    //get the id
+    var id = req.params.id;
+    //validate the id -> validate the id? return 404
+    if(!ObjectID.isValid(id))
+    {
+        res.status(404).send();
+    }
+
+    //remove todo by id
+
+    Todo.findOneAndDelete(id).then((todo)=> {
+        //success
+        //if no doc, send 404
+        if(!todo)
+            return res.status(404).send();
+         //if doc, send doc back with 200
+        res.send({todo});
+      
+    }).catch((e)=> {
+        res.status(400).send();
+    });
+
+    
+});
+
+app.patch('/todos/:id',(req,res)=>{
+    var id = req.params.id;
+
+    var body = _.pick(req.body,['text'],['completed']);
+
+    if(!ObjectID.isValid(id))
+    {
+        res.status(404).send();
+    }
+
+    if(_.isBoolean(body.completed) && body.completed)
+    {
+        body.completedAt = new Date().getTime();
+    }
+    else
+    {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findOneAndUpdate(id,{$set : body}, {new : true}).then ((todo) => {
+        if(!todo)
+        {
+            return res.status(404).send();
+        }
+        
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    })
 
 });
 
