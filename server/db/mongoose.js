15f0826d71140c1feaa1faa49e642aa8ce60678b
nathanielpascual <nath_pascual@yahoo.com>
2018-10-07 18:06:01 +0800

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,10 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp',{ useNewUrlParser: true },(error)=>{
+mongoose.connect(process.env.MONGODB_URI ,{ useNewUrlParser: true },(error)=>{
     //console.log(error);
 });
 
 module.exports = {mongoose}
+
+//process.env.NODE_ENV === 'production'
